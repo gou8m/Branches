24fc8512da3da02138e9b47fd259dc1880833320
Goutham Kumar Aare <gouthamreddy671@gmail.com>
2025-04-10 17:25:18 +0530

    updated instance type

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ def lambda_handler(event, context):
   client = boto3.client('ec2')
   response = client.run_instances(
     ImageId='ami-0614680123427b75e',
-    InstanceType='t3.micro',
+    InstanceType='t2.micro',
     KeyName='aws',
     MaxCount=1,
     MinCount=1
